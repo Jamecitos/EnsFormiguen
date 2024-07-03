dc7f2fd8f4b3cc199ac3b264da89ac7989fda248
Paugaex <u1968545@campus.udg.edu>
2024-07-03 20:13:54 +0200

    Crear pulsadores generales para menu.

---
+++ menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://Combate.tscn")
+
+
+func _on_options_pressed():
+	pass # Replace with function body.
+
+
+func _on_language_pressed():
+	pass # Replace with function body.
+
+
+func _on_exit_pressed():
+	get_tree().quit()
+	
+
+
+func _on_continue_pressed():
+	pass # Replace with function body.
