5826823dc1762c086e7aa9ffb78768ff3ea0fefa
Paugaex <u1968545@campus.udg.edu>
2024-07-03 22:52:07 +0200

    Crear fondo de menu opciones y ordenar scripts en carpetas

--- MenuPrincipal.gd
+++ MenuPrincipal.gd
@@ -4,9 +4,6 @@ func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Combate.tscn")
 
 
-func _on_options_pressed():
-	pass # Replace with function body.
-
 
 func _on_language_pressed():
 	pass # Replace with function body.
@@ -16,3 +13,6 @@ func _on_exit_pressed():
 	get_tree().quit()
 	
 
+
+func _on_options_pressed():	
+	get_tree().change_scene_to_file("res://MenuOpciones.tscn")
--- TituloOpciones.gd
+++ TituloOpciones.gd
@@ -1,9 +1,9 @@
-extends Button
+extends TextEdit
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	text= "LANGUAGE"
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
