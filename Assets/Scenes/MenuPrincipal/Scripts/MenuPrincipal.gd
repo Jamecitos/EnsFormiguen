cb8f3dba5fb3ce1f94112e23befc981d3fa190e2
Paugaex <u1968545@campus.udg.edu>
2024-07-04 12:56:32 +0200

    Comentar codigo

--- Assets/Scenes/MenuPrincipal/Scripts/MenuPrincipal.gd
+++ Assets/Scenes/MenuPrincipal/Scripts/MenuPrincipal.gd
@@ -1,10 +1,12 @@
 extends Control
 
 func _ready():
+	# Reproduce la música del menú. Asume que hay un nodo de audio llamado 'MusicaMenu'.
 	$MusicaMenu.play()
 
 
 func _on_play_pressed():
+	# Cambia la escena actual a 'Combate.tscn'. Se asume que este archivo existe en el directorio 'res://'.
 	get_tree().change_scene_to_file("res://Combate.tscn")
 	
 
@@ -15,9 +17,11 @@ func _on_language_pressed():
 
 
 func _on_exit_pressed():
+	# Sale del juego o cierra la aplicación.
 	get_tree().quit()
 	
 
 
 func _on_options_pressed():	
-	get_tree().change_scene_to_file("res://MenuOpciones.tscn")
+		# Cambia la escena actual a 'MenuOpciones.tscn'. Se asume que este archivo existe en el directorio 'res://'.
+		get_tree().change_scene_to_file("res://MenuOpciones.tscn")
