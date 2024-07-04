b4f0ae2e165fa0a7e33f619b9edb6fabaef5924c
Paugaex <u1968545@campus.udg.edu>
2024-07-04 03:03:32 +0200

    agregar opciones de pantalla (MenuOpciones completado)

--- Assets/Scenes/MenuOpciones/Scripts/MenuOpciones.gd
+++ Assets/Scenes/MenuOpciones/Scripts/MenuOpciones.gd
@@ -22,3 +22,22 @@ func _on_music_value_changed(value):
 
 func _on_sound_fx_value_changed(value):
 	volume(2,value)
+
+
+func _on_full_screen_toggled(toggled_on):
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN) 
+	else :
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+
+func _on_borderless_toggled(toggled_on):
+	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, toggled_on)
+
+
+func _on_v_sync_item_selected(index):
+	DisplayServer.window_set_vsync_mode(index)
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://MenuPrincipal.tscn")
